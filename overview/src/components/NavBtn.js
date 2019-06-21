c19d9e324004debf2cf0d785e3b9400c84b4e81b
silashuereca <silasgranthuereca@gmail.com>
2019-06-21 11:46:44 -0500

    Finished the side bar navigation

---
+++ overview/src/components/NavBtn.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavBtn() {
+  return (
+    <div id="mySidenav">
+      <a href="#" id="workout">
+        Workout
+      </a>
+      <a href="#" id="finances">
+        Finance
+      </a>
+      <a href="#" id="vacation">
+        Vacation
+      </a>
+    </div>
+  );
+}
+
+export default NavBtn;
